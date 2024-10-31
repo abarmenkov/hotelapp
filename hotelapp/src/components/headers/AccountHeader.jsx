4e0d46541303b396bcb9515f1e853991b297c6ad
Andrey Barmenkov <barmenkov.av@yandex.ru>
2024-10-31 17:53:27 +0300

    added dotted menu to account-header

--- hotelapp/src/components/headers/AccountHeader.jsx
+++ hotelapp/src/components/headers/AccountHeader.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { Appbar, useTheme } from "react-native-paper";
+import React, { useState } from "react";
+import { Appbar, useTheme, Tooltip, Menu, Divider } from "react-native-paper";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { useTranslation } from "react-i18next";
+import { Alert } from "react-native";
 
 export const AccountHeader = ({ navigation, title }) => {
   const { t } = useTranslation();
   const theme = useTheme();
+  const [visible, setVisible] = useState(false);
+
+  const closeMenu = () => setVisible(false);
+  const openMenu = (v) => setVisible(true);
+
   return (
     <Appbar.Header style={theme.colors.onSecondary}>
       <Appbar.BackAction size={24} onPress={navigation.goBack} />
@@ -32,12 +38,49 @@ export const AccountHeader = ({ navigation, title }) => {
           console.log("search");
         }}
       />
-      <Appbar.Action
-        icon="dots-vertical"
-        onPress={() => {
-          console.log("dots");
-        }}
-      />
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <Tooltip title="dots">
+            <Appbar.Action icon="dots-vertical" onPress={openMenu} />
+          </Tooltip>
+        }
+        anchorPosition="bottom"
+        mode="elevated"
+        elevation={5}
+        statusBarHeight={15}
+      >
+        <Menu.Item
+          onPress={() => {
+            Alert.alert("Action : ", "Print");
+          }}
+          title="Print"
+        />
+        <Divider />
+
+        <Menu.Item
+          onPress={() => {
+            Alert.alert("Action : ", "Forward");
+          }}
+          title="Forward"
+          disabled
+        />
+        <Menu.Item
+          onPress={() => {
+            Alert.alert("Action : ", "Backward");
+          }}
+          title="Backward"
+          disabled
+        />
+        <Menu.Item
+          onPress={() => {
+            Alert.alert("Action :", "Save");
+          }}
+          title="Save"
+          disabled
+        />
+      </Menu>
     </Appbar.Header>
   );
 };
